e2588e5ec2182046ec6691013ea0dc4fd712a001
Lars Passic <larspassic@outlook.com>
2021-04-29 19:05:41 -0700

    Created HelloWorld in labs for DataBinding labs in class

---
+++ Labs/HelloWorld/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //this.WindowState = WindowState.Maximized;
+
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Submitting password: {uxPassword.Text}");
+        }
+
+        private void uxPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckForCharacters();
+        }
+
+        private void uxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckForCharacters();
+        }
+
+        private void CheckForCharacters()
+        {
+            //Check if both uxPassword and uxName have something in them, if so, enable the button
+            if (uxName.Text != "" && uxPassword.Text != "")
+            {
+                this.uxSubmit.IsEnabled = true;
+            }
+            
+            //Else if statement to turn the button off
+            else if (uxName.Text == "" || uxPassword.Text == "")
+            {
+                this.uxSubmit.IsEnabled = false;
+            }
+        }
+    }
+}
