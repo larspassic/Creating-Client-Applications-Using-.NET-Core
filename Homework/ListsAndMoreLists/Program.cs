0481d2adba1b0e6b8df58cab8916d822aac8b0ec
Lars Passic <larspassic@outlook.com>
2021-05-06 17:32:02 -0700

    Possible finished product for homework 2
    Added more test users so that something would output at the end.

--- Homework/ListsAndMoreLists/Program.cs
+++ Homework/ListsAndMoreLists/Program.cs
@@ -17,18 +17,20 @@ namespace ListsAndMoreLists
             users.Add(new Models.User { Name = "Dave", Password = "hello" });
             users.Add(new Models.User { Name = "Steve", Password = "steve" });
             users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+            users.Add(new Models.User { Name = "John", Password = "d$Mx&3#" });
+            users.Add(new Models.User { Name = "Mary", Password = "asdfJKL1" });
 
             //Checked the collection exercise - looks like it is okay to do the homework in main.
 
-            //Use System.Linq for all the requirements. IE.Don't use a for/foreach loop to manipulate the users list.
+                //Use System.Linq for all the requirements. IE.Don't use a for/foreach loop to manipulate the users list.
 
-            ////////////////////
-            //1.Display to the console, the names of the users where the password is "hello" and you can use a foreach here only.
-            ////////////////////
-            //Hint: Where
+                ////////////////////
+                //1.Display to the console, the names of the users where the password is "hello" and you can use a foreach here only.
+                //Hint: Where
+                ////////////////////
 
-            //Use LINQ query to find the results
-            var results = users.Where(t => t.Password == "hello");
+                //Use LINQ query to find the results
+                var results = users.Where(t => t.Password == "hello");
 
             //Announce the result to the console
             Console.WriteLine($"Users where password was \"hello\" "); //use escape characters to say "hello"
@@ -37,28 +39,47 @@ namespace ListsAndMoreLists
             foreach (var result in results)
             {
                 Console.WriteLine($"Found user {result.Name}");
-
             }
+
+
 
             ////////////////////
             //2.Delete any passwords that are the lower-cased version of their Name. Do not just look for "steve".It has to be data - driven.
-            ////////////////////
             //Hint: Remove or RemoveAll
+            ////////////////////
+
 
             //Use a find query to find users where the name matches the lower case version of their name
             var passwordresults = users.Find(t => (t.Name).ToLower() == t.Password);
 
+            //Explain to the console what we are going to do
+            Console.WriteLine($"Removing users for their password matching their name: {passwordresults}");
+
+            //Actually remove the users that we found
+            users.Remove(passwordresults);
+
+
+
 
             ////////////////////
             //3.Delete the first User that has the password "hello".
-            ////////////////////
             //Hint: First or FirstOrDefault
+            ////////////////////
+
+            //Find the first user where password is equal to "hello"
             var helloresults = users.Find(t => t.Password == "hello");
+
+            //Explain to the console what we are going to do
+            Console.WriteLine($"Removing the first user who's password is \"hello\": {helloresults}");
+
+            //Actually remove the user
+            users.Remove(helloresults);
 
             ////////////////////
             //4.Display to the console the remaining users with their Name and Password.
-            ////////////////////
             //Hint: ForEach
+            ////////////////////
+
             var finalresults = users.FindAll(a => a != null);
 
             foreach (var result in finalresults)
