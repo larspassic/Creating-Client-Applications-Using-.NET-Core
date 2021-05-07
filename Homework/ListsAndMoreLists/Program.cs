6fc34bc6c104c31757f5c3bb53bbd3dcdf367026
Lars Passic <larspassic@outlook.com>
2021-05-06 17:58:07 -0700

    Finishing Homework 2
    Updates to everything, got it working.

--- Homework/ListsAndMoreLists/Program.cs
+++ Homework/ListsAndMoreLists/Program.cs
@@ -22,15 +22,15 @@ namespace ListsAndMoreLists
 
             //Checked the collection exercise - looks like it is okay to do the homework in main.
 
-                //Use System.Linq for all the requirements. IE.Don't use a for/foreach loop to manipulate the users list.
+            //Use System.Linq for all the requirements. IE.Don't use a for/foreach loop to manipulate the users list.
 
-                ////////////////////
-                //1.Display to the console, the names of the users where the password is "hello" and you can use a foreach here only.
-                //Hint: Where
-                ////////////////////
+            ////////////////////
+            //1.Display to the console, the names of the users where the password is "hello" and you can use a foreach here only.
+            //Hint: Where
+            ////////////////////
 
-                //Use LINQ query to find the results
-                var results = users.Where(t => t.Password == "hello");
+            //Use LINQ query to find the results
+            var results = users.Where(t => t.Password == "hello");
 
             //Announce the result to the console
             Console.WriteLine($"Users where password was \"hello\" "); //use escape characters to say "hello"
@@ -38,9 +38,11 @@ namespace ListsAndMoreLists
             //Allowed to use foreach to display each result out
             foreach (var result in results)
             {
-                Console.WriteLine($"Found user {result.Name}");
+                Console.WriteLine($"Found user {result.Name}, {result.Password}");
             }
 
+            //Extra space
+            Console.WriteLine();
 
 
             ////////////////////
@@ -50,14 +52,16 @@ namespace ListsAndMoreLists
 
 
             //Use a find query to find users where the name matches the lower case version of their name
-            var passwordresults = users.Find(t => (t.Name).ToLower() == t.Password);
+            var passwordresults = users.FindAll(t => (t.Name).ToLower() == t.Password);
 
             //Explain to the console what we are going to do
-            Console.WriteLine($"Removing users for their password matching their name: {passwordresults}");
+            Console.WriteLine($"Removing users for their password matching their name, starting with: {passwordresults[0].Name}");
 
             //Actually remove the users that we found
-            users.Remove(passwordresults);
+            users.RemoveAll(t => (t.Name).ToLower() == t.Password);
 
+            //Extra space
+            Console.WriteLine();
 
 
 
@@ -70,23 +74,39 @@ namespace ListsAndMoreLists
             var helloresults = users.Find(t => t.Password == "hello");
 
             //Explain to the console what we are going to do
-            Console.WriteLine($"Removing the first user who's password is \"hello\": {helloresults}");
+            Console.WriteLine($"Removing the first user who's password is \"hello\": {helloresults.Name}, {helloresults.Password}");
 
             //Actually remove the user
             users.Remove(helloresults);
+
+            //Extra space
+            Console.WriteLine();
+
+
 
             ////////////////////
             //4.Display to the console the remaining users with their Name and Password.
             //Hint: ForEach
             ////////////////////
 
+            //Pull back all of the users
             var finalresults = users.FindAll(a => a != null);
 
+            //Announce to the console what we are about to do
+            Console.WriteLine($"Displaying all remaining users:");
+
+            //Loop through each remaining user
             foreach (var result in finalresults)
             {
-                Console.WriteLine($"Found user {result.Name}");
+                Console.WriteLine($"Found user {result.Name}, {result.Password}");
 
             }
+
+            //Extra space
+            Console.WriteLine();
+
+            //Keep the program running
+            Console.ReadLine();
         }
     }
 }
