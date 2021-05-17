8ade8b76e83f0941674b91fa016d039c715ff10f
Lars Passic <43658463+larspassic@users.noreply.github.com>
2021-05-16 22:43:16 -0700

    Implemented the logic for checking for a US or CAN Zip Code.

--- Homework/ZipCode TextBox/MainWindow.xaml.cs
+++ Homework/ZipCode TextBox/MainWindow.xaml.cs
@@ -31,7 +31,53 @@ namespace ZipCode_TextBox
         //Event handler for every time text inside the text box is changed.
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            //Code to validate Zip Codes goes here
+            //Code to validate Zip Codes goes here - ready, begin!
+
+            //Pull string in from text box
+            string textBoxContents = TextBoxZipCode.Text;
+
+            //Create some variables to track progress
+            bool usZipCodeFound;
+            bool canZipCodeFound;
+
+            //Store the results of the actual checks
+            usZipCodeFound = CheckForUSZipCode(textBoxContents);
+            canZipCodeFound = CheckForCanZipCode(textBoxContents);
+
+            //Actually act on the test results
+            //Code for if a Zip code was found
+            if (usZipCodeFound || canZipCodeFound)
+            {
+                //If there was a previous attempt, the error message may still be showing
+                //Since Zip Code was found and we are successful now, need to disable it
+                TextBlockErrorMessage.IsEnabled = false;
+                
+                //Enable the button
+                ButtonSubmit.IsEnabled = true;
+
+
+            }
+            //Code for if a Zip code was NOT found
+            if (usZipCodeFound && canZipCodeFound == false)
+            {
+                //Since Zip code was not found, need to enable the error message
+                TextBlockErrorMessage.IsEnabled = true;
+
+                //If there was previously a valid Zip, the submit button may have been enabled
+                //Since Zip code was not found, need to disable the submit button
+                ButtonSubmit.IsEnabled = false;
+            }
+
+        }
+
+        private bool CheckForCanZipCode(string textBoxContents)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CheckForUSZipCode(string textBoxContents)
+        {
+            throw new NotImplementedException();
         }
 
         //Event handler for when submit button is clicked.
