089430b78e8a3a7fac4ebdfa6e736a0944544eef
Lars Passic <larspassic@outlook.com>
2021-05-20 18:14:08 -0700

    Files from instructor

---
+++ Labs/HelloWorld from instructor/MenuWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnNew_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("New command");
+        }
+        private void OnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        // ADD CAN EXECUTE
+        // Click Ctrl-N to execute the shortcut.
+        private void OnNew_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            // Set this to false if the New command is not available
+            //e.CanExecute = true;
+            // NOTE: if set to false instead of true, it will disable the "New"
+            e.CanExecute = false;
+        }
+    }
+}
