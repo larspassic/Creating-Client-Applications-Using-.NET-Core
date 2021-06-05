2beb9755b1d8769dc68fe2764ac19380497b6075
Lars Passic <43658463+larspassic@users.noreply.github.com>
2021-06-04 22:31:48 -0700

    Worked on some code for a fun experiment

---
+++ Labs/NumberCounter/NumberCounter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NumberCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long longMin = long.MinValue;
+            long longMax = long.MaxValue;
+
+            Console.WriteLine($"Welcome to the number counting name!");
+            Console.WriteLine($"Please enter a number between 0 and {longMax}:");
+
+            string userInput = Console.ReadLine();
+            long userInputNumber = long.Parse(userInput);
+            bool wantToQuit = false;
+
+            long currentNumber = 0;
+            do
+            {
+                if (userInputNumber == 24)
+                {
+                    Console.WriteLine($"Lee loves Terese!");
+                }
+                
+                while (currentNumber < userInputNumber+1)
+                {
+                    Console.WriteLine($"{currentNumber}");
+                    currentNumber++;
+                }
+
+                Console.WriteLine($"Do you want to count again? (Y/N):");
+                string countAgainInput = Console.ReadLine();
+
+                if (countAgainInput.ToLower() == "n")
+                {
+                    wantToQuit = true;
+                }
+            }
+            while (wantToQuit == false);
+        }
+    }
+}
