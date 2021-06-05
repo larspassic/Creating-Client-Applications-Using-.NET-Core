9ae4b873ea0208ce0aaa12e7bbf5b93ffa365079
Lars Passic <larspassic@outlook.com>
2021-06-05 13:39:53 -0700

    Created Birthday Card Generator homework
    About halfway done

---
+++ Homework/Birthday Card Generator/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Birthday_Card_Generator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Birthday_Card_Generator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BirthdayCardForm()
+        {
+            return View(new BirthdayCardInput());
+        }
+    }
+}
